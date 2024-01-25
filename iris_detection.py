0d0a36441e3d5c7d4ae7bed972b33f9d86a302f2
Aravin-S <73080952+Aravin-S@users.noreply.github.com>
2024-01-25 00:11:11 -0500

    mouse movement and clicking work

--- iris_detection.py
+++ iris_detection.py
@@ -5,6 +5,7 @@ import datetime
 
 cam = cv2.VideoCapture(0)
 face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
+screen_w, screen_h = pyautogui.size()
 
 while True:
     _, frame = cam.read()
@@ -16,13 +17,21 @@ while True:
 
     if landmark_points:
         landmarks = landmark_points[0].landmark
+        for id, landmark in enumerate(landmarks[474:478]):
+            x = int(landmark.x * frame_w)
+            y = int(landmark.y * frame_h)
+            cv2.circle(frame, (x, y), 3, (0, 255, 0))
+            if id == 1:
+                screen_x = screen_w * landmark.x
+                screen_y = screen_h * landmark.y
+                pyautogui.moveTo(screen_x, screen_y)
         left = [landmarks[145], landmarks[159]]
         for landmark in left:
             x = int(landmark.x * frame_w)
             y = int(landmark.y * frame_h)
             cv2.circle(frame, (x, y), 3, (0, 255, 255))
 
-        if (left[0].y - left[1].y) < 0.004:
+        if (left[0].y - left[1].y) < 0.01:
             pyautogui.click()
             click_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             print(f"Click registered at {click_time}")
